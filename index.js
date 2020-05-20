935f756dc5676a822a8628c0d64727bf2321381f
Alius <alexa.glinsoo@gmail.com>
2020-05-20 18:16:35 -0300

    aumentando comandos

--- index.js
+++ index.js
@@ -3,18 +3,31 @@ const bot = new Discord.Client();
 const config = require('./config.json');
 
 bot.on('message', message => {
-    if (message.content.startsWith('!ping')) {
+    if (message.content.startsWith('/ping')) {
         message.channel.send('pong!');
     }
 
 
-    if (message.content.startsWith("!roleta")) {
+    if (message.content.startsWith("/roleta")) {
         randomNumber = Math.floor(Math.random() * (6 - 1) + 1);
         if (randomNumber == 2) {
             message.reply("Morreu!");
         } else {
             message.reply("Sobreviveu!");
         }
+    }
+
+    if (message.content.startsWith("/gaydar")) {
+        randomNumber = Math.floor(Math.random() * (6 - 1) + 1);
+        if (randomNumber == 2) {
+            message.reply("Você não é gay!");
+        } else {
+            message.reply("Você é gay!");
+        }
+    }
+
+    if (message.content.startsWith('/help')) {
+        message.channel.send('Comandos registrados do sistema: \n/ping \n/roleta \n/gaydar');
     }
     
 });
